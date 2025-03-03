28b6650460d68da34733d470e3bb8b0070cf0bf7
Jelle Hak <jellehak@gmail.com>
2025-03-03 12:56:52 +0100

    chore: update main entry point to start.js and modify start script

---
+++ start.js
@@ -0,0 +1,14 @@
+import { debug } from 'book';
+import CreateServer from './server.js';
+
+const server = CreateServer({
+    // max_tcp_sockets: argv['max-sockets'],
+    // secure: argv.secure,
+    // domain: argv.domain,
+});
+
+const PORT = process.env.PORT || 80;
+
+server.listen(PORT, () => {
+    debug('server listening on port: %d', server.address().port);
+});
